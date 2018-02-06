525739c45448a70aacb6ab9a5fcd7e5bbe318fa7
Chris Hutchinson <chrisbond19@me.com>
2018-02-06 15:06:25 -0600

    Added Spotify search results, working on Twitter search results

--- liri.js
+++ liri.js
@@ -1,22 +1,25 @@
-var request = require("dotenv").config();
+require("dotenv").config();
 
-var key = require("./key.js");
+var Spotify = require('node-spotify-api');
 
-var command = process.agrv[2];
+var keys = require("./key.js");
 
 var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+// var client = new Twitter(keys.twitter);
 
-
-request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
-
-  // If there were no errors and the response code was 200 (i.e. the request was successful)...
-  if (!error && response.statusCode === 200) {
-
-    // Then we print out the imdbRating
-    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-  }
-});
+var command = process.argv[3];
+var song = process.argv[2];
 if (command === "spotify-this-song"){
-	console.log("works");
+
+ 
+	spotify.search({ type: 'track', query: song, limit: 1 }, function(err, data) {
+  		if (err) {
+    		return console.log('Error occurred: ' + err);
+  		}
+  		
+  		for(var i = 0; i < data.tracks.items[0]["album"]["artists"].length; i++) {
+  			console.log(data.tracks.items[0]["album"]["artists"][i].name)
+  			console.log("======================")
+  		}
+	});
 }
